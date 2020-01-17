e8895b8f63853776c439afa7dee6e9b380367692
RogueLogix <roguelogix@roguelogix.net>
2020-01-16 22:14:56 -0800

    added partial ROBN test coverage

--- RogueLib/ROBN/test/test.cpp
+++ RogueLib/ROBN/test/test.cpp
@@ -9,9 +9,11 @@
 #define BOOST_TEST_MODULE GenericBinary
 
 #include <boost/test/unit_test.hpp>
+#include <random>
 
 using namespace RogueLib::ROBN;
 
+std::default_random_engine generator;
 
 BOOST_AUTO_TEST_CASE(stringEncode) {
     srand(std::chrono::system_clock::now().time_since_epoch().count());
@@ -81,12 +83,17 @@ BOOST_AUTO_TEST_CASE(boolDecode) {
 //
 BOOST_AUTO_TEST_CASE(int8Encode) {
     std::int8_t val = 0;
-
+    do {
+        auto bytes = toROBN(val);
+        BOOST_CHECK(bytes.size() == 2);
+        BOOST_CHECK(bytes[0] == (std::uint8_t(Type::Int8) | std::uint8_t(Endianness::NATIVE)));
+        BOOST_CHECK(bytes[1] == std::uint8_t(val));
+    } while (val++ != 0);
 }
 
 BOOST_AUTO_TEST_CASE(int8Decode) {
     std::vector<std::uint8_t> decodeTestBytes;
-    decodeTestBytes.emplace_back(Type::Bool);
+    decodeTestBytes.emplace_back(Type::Int8);
     decodeTestBytes.emplace_back(0);
     BOOST_CHECK(!fromROBN<std::int8_t>(decodeTestBytes));
 
@@ -98,9 +105,247 @@ BOOST_AUTO_TEST_CASE(int8Decode) {
 }
 
 BOOST_AUTO_TEST_CASE(int16Encode) {
-
+    std::int16_t val = 0;
+    auto* bytePtr = reinterpret_cast<uint8_t*>(&val);
+    do {
+        auto bytes = toROBN(val);
+        BOOST_CHECK(bytes.size() == 3);
+        BOOST_CHECK(bytes[0] == (std::uint8_t(Type::Int16) | std::uint8_t(Endianness::NATIVE)));
+        BOOST_CHECK(bytes[1] == bytePtr[0]);
+        BOOST_CHECK(bytes[2] == bytePtr[1]);
+    } while (++val != 0);
 }
 
 BOOST_AUTO_TEST_CASE(int16Decode) {
+    std::int16_t val = 0;
+    do {
+        auto bytes = toROBN(val);
+        BOOST_CHECK_MESSAGE(val == fromROBN<std::int16_t>(bytes), std::to_string(val));
+        bytes[0] ^= Endianness::BIG;
+        bytes[1] ^= bytes[2];
+        bytes[2] ^= bytes[1];
+        bytes[1] ^= bytes[2];
+        BOOST_CHECK_MESSAGE(val == fromROBN<std::int16_t>(bytes), std::to_string(val));
+    } while (++val != 0);
+}
 
+BOOST_AUTO_TEST_CASE(int32Encode) {
+    auto runCheck = [](auto val) {
+        auto* bytePtr = reinterpret_cast<uint8_t*>(&val);
+        auto bytes = toROBN(val);
+        BOOST_CHECK(bytes.size() == 5);
+        BOOST_CHECK(bytes[0] == (std::uint8_t(Type::Int32) | std::uint8_t(Endianness::NATIVE)));
+        BOOST_CHECK(bytes[1] == bytePtr[0]);
+        BOOST_CHECK(bytes[2] == bytePtr[1]);
+        BOOST_CHECK(bytes[3] == bytePtr[2]);
+        BOOST_CHECK(bytes[4] == bytePtr[3]);
+    };
+
+    std::uniform_int_distribution<std::int32_t> distribution(1, pow(2, 16));
+
+    for (std::int32_t i = INT32_MIN; i < 0; i += distribution(generator)) {
+        runCheck(i);
+    }
+    for (std::int32_t i = 1; i > 0; i += distribution(generator)) {
+        runCheck(i);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(int32Decode) {
+    auto runCheck = [](auto val) {
+        auto bytes = toROBN(val);
+        BOOST_CHECK_MESSAGE(val == fromROBN<decltype(val)>(bytes), std::to_string(val));
+        bytes[0] ^= Endianness::BIG;
+
+        bytes[2] ^= bytes[3];
+        bytes[3] ^= bytes[2];
+        bytes[2] ^= bytes[3];
+
+        bytes[1] ^= bytes[4];
+        bytes[4] ^= bytes[1];
+        bytes[1] ^= bytes[4];
+
+        BOOST_CHECK_MESSAGE(val == fromROBN<decltype(val)>(bytes), std::to_string(val));
+    };
+
+    std::uniform_int_distribution<std::int32_t> distribution(1, pow(2, 16));
+
+    for (std::int32_t i = INT32_MIN; i < 0; i += distribution(generator)) {
+        runCheck(i);
+    }
+    for (std::int32_t i = 1; i > 0; i += distribution(generator)) {
+        runCheck(i);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(int64Encode) {
+    auto runCheck = [](auto val) {
+        auto* bytePtr = reinterpret_cast<uint8_t*>(&val);
+        auto bytes = toROBN(val);
+        BOOST_CHECK(bytes.size() == 9);
+        BOOST_CHECK(bytes[0] == (std::uint8_t(Type::Int64) | std::uint8_t(Endianness::NATIVE)));
+        BOOST_CHECK(bytes[1] == bytePtr[0]);
+        BOOST_CHECK(bytes[2] == bytePtr[1]);
+        BOOST_CHECK(bytes[3] == bytePtr[2]);
+        BOOST_CHECK(bytes[4] == bytePtr[3]);
+        BOOST_CHECK(bytes[5] == bytePtr[4]);
+        BOOST_CHECK(bytes[6] == bytePtr[5]);
+        BOOST_CHECK(bytes[7] == bytePtr[6]);
+        BOOST_CHECK(bytes[8] == bytePtr[7]);
+    };
+
+    std::uniform_int_distribution<std::int64_t> distribution(1, pow(2, 48));
+
+    for (std::int64_t i = INT64_MIN; i < 0; i += distribution(generator)) {
+        runCheck(i);
+    }
+    for (std::int64_t i = 1; i > 0; i += distribution(generator)) {
+        runCheck(i);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(int64Decode) {
+    auto runCheck = [](auto val) {
+        auto bytes = toROBN(val);
+        BOOST_CHECK_MESSAGE(val == fromROBN<decltype(val)>(bytes), std::to_string(val));
+        bytes[0] ^= Endianness::BIG;
+
+        bytes[1] ^= bytes[8];
+        bytes[8] ^= bytes[1];
+        bytes[1] ^= bytes[8];
+
+        bytes[2] ^= bytes[7];
+        bytes[7] ^= bytes[2];
+        bytes[2] ^= bytes[7];
+
+        bytes[3] ^= bytes[6];
+        bytes[6] ^= bytes[3];
+        bytes[3] ^= bytes[6];
+
+        bytes[4] ^= bytes[5];
+        bytes[5] ^= bytes[4];
+        bytes[4] ^= bytes[5];
+
+        BOOST_CHECK_MESSAGE(val == fromROBN<decltype(val)>(bytes), std::to_string(val));
+    };
+
+    std::uniform_int_distribution<std::int64_t> distribution(1, pow(2, 48));
+
+    for (std::int64_t i = INT64_MIN; i < 0; i += distribution(generator)) {
+        runCheck(i);
+    }
+    for (std::int64_t i = 1; i > 0; i += distribution(generator)) {
+        runCheck(i);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(int128Encode) {
+    auto runCheck = [](auto val) {
+        auto* bytePtr = reinterpret_cast<uint8_t*>(&val);
+        auto bytes = toROBN(val);
+        BOOST_CHECK(bytes.size() == 17);
+        BOOST_CHECK(bytes[0] == (std::uint8_t(Type::Int128) | std::uint8_t(Endianness::NATIVE)));
+        BOOST_CHECK(bytes[1] == bytePtr[0]);
+        BOOST_CHECK(bytes[2] == bytePtr[1]);
+        BOOST_CHECK(bytes[3] == bytePtr[2]);
+        BOOST_CHECK(bytes[4] == bytePtr[3]);
+        BOOST_CHECK(bytes[5] == bytePtr[4]);
+        BOOST_CHECK(bytes[6] == bytePtr[5]);
+        BOOST_CHECK(bytes[7] == bytePtr[6]);
+        BOOST_CHECK(bytes[8] == bytePtr[7]);
+        BOOST_CHECK(bytes[9] == bytePtr[8]);
+        BOOST_CHECK(bytes[10] == bytePtr[9]);
+        BOOST_CHECK(bytes[11] == bytePtr[10]);
+        BOOST_CHECK(bytes[12] == bytePtr[11]);
+        BOOST_CHECK(bytes[13] == bytePtr[12]);
+        BOOST_CHECK(bytes[14] == bytePtr[13]);
+        BOOST_CHECK(bytes[15] == bytePtr[14]);
+        BOOST_CHECK(bytes[16] == bytePtr[15]);
+    };
+
+    __int128 randmax = 0;
+    randmax |= UINT64_MAX;
+    randmax <<= 63;
+    randmax |= UINT64_MAX;
+
+    __int128 randmin = 0;
+    randmin |= UINT64_MAX;
+    randmin <<= 52;
+    randmin |= UINT64_MAX;
+
+    std::uniform_int_distribution<__int128> distribution(randmin, randmax);
+
+    __int128 min = 1;
+    min <<= 127;
+
+    for (__int128 i = min; i < 0; i += distribution(generator)) {
+        runCheck(i);
+    }
+    for (__int128 i = 1; i > 0; i += distribution(generator)) {
+        runCheck(i);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(int128Decode) {
+    auto runCheck = [](auto val) {
+        auto bytes = toROBN(val);
+        BOOST_CHECK(val == fromROBN<decltype(val)>(bytes));
+        bytes[0] ^= Endianness::BIG;
+
+        bytes[1] ^= bytes[16];
+        bytes[16] ^= bytes[1];
+        bytes[1] ^= bytes[16];
+
+        bytes[2] ^= bytes[15];
+        bytes[15] ^= bytes[2];
+        bytes[2] ^= bytes[15];
+
+        bytes[3] ^= bytes[14];
+        bytes[14] ^= bytes[3];
+        bytes[3] ^= bytes[14];
+
+        bytes[4] ^= bytes[13];
+        bytes[13] ^= bytes[4];
+        bytes[4] ^= bytes[13];
+
+        bytes[5] ^= bytes[12];
+        bytes[12] ^= bytes[5];
+        bytes[5] ^= bytes[12];
+
+        bytes[6] ^= bytes[11];
+        bytes[11] ^= bytes[6];
+        bytes[6] ^= bytes[11];
+
+        bytes[7] ^= bytes[10];
+        bytes[10] ^= bytes[7];
+        bytes[7] ^= bytes[10];
+
+        bytes[8] ^= bytes[9];
+        bytes[9] ^= bytes[8];
+        bytes[8] ^= bytes[9];
+
+        BOOST_CHECK(val == fromROBN<decltype(val)>(bytes));
+    };
+
+    __int128 randmax = 0;
+    randmax |= UINT64_MAX;
+    randmax <<= 63;
+    randmax |= UINT64_MAX;
+
+    __int128 randmin = 0;
+    randmin |= UINT64_MAX;
+    randmin <<= 52;
+    randmin |= UINT64_MAX;
+
+    std::uniform_int_distribution<__int128> distribution(randmin, randmax);
+
+    __int128 min = 1;
+    min <<= 127;
+
+    for (__int128 i = min; i < 0; i += distribution(generator)) {
+        runCheck(i);
+    }
+    for (__int128 i = 1; i > 0; i += distribution(generator)) {
+        runCheck(i);
+    }
 }
